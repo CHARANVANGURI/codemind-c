0f21341e7b5b22a0ca31075aa774eb5db974657f
CHARANVANGURI <147791758+CHARANVANGURI@users.noreply.github.com>
2023-12-15 16:43:57 +0530

    #include<stdio.h> #include<limits.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n],sum=0;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(i%2==0)         sum=sum+arr[i];     }     printf("%d",sum); }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n],sum=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(i%2==0)
+        sum=sum+arr[i];
+    }
+    printf("%d",sum);
+}
