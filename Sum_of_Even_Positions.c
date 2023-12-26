09fc26e601329bf50ab6a657b063be0853279be7
CHARANVANGURI <147791758+CHARANVANGURI@users.noreply.github.com>
2023-12-26 10:35:02 +0530

    #include<stdio.h> int main(){     int n,s=0;     scanf("%d",&n);    int arr[n];     for(int i=0;i<n;i++){         scanf("%d",&arr[i]);          if(i%2==0){     s=s+arr[i]; }                  //if condition should bi in the loop.     }           //i-->index & arr[i] --> no. in that index.
    printf("%d",s);
    }

--- Sum_of_Even_Positions.c
+++ Sum_of_Even_Positions.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-#include<limits.h>
-int main()
-{
-    int n,i;
+int main(){
+    int n,s=0;
     scanf("%d",&n);
-    int arr[n],sum=0;
-    for(i=0;i<n;i++)
-    {
+   int arr[n];
+    for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
-        if(i%2==0)
-        sum=sum+arr[i];
-    }
-    printf("%d",sum);
+         if(i%2==0){
+    s=s+arr[i];
+}                  //if condition should bi in the loop.
+    }           //i-->index & arr[i] --> no. in that index.
+   
+printf("%d",s);
 }
