7eb2942c1448b0a4087989a1001ebd4c91bca4c0
CHARANVANGURI <147791758+CHARANVANGURI@users.noreply.github.com>
2023-12-27 10:55:47 +0530

    #include<stdio.h> int main(){     int n,num, count = 0;     scanf("%d",&n);     int arr[n];     	for(int i = 0 ; i < n ; i++){     		 scanf("%d",&arr[i]); 		} 		scanf("%i" , &num);     for(int i=0;i<n;i++){
            	if(num==arr[i]){
            		count++;
    				}
            	}
            		printf("%d",count);
    			}

---
+++ Count_of_given_element_in_an_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,num, count = 0;
+    scanf("%d",&n);
+    int arr[n];
+    	for(int i = 0 ; i < n ; i++){
+    		 scanf("%d",&arr[i]);
+		}
+		scanf("%i" , &num);
+    for(int i=0;i<n;i++){
+       
+        	if(num==arr[i]){
+        		count++;
+				}
+        	}
+        		printf("%d",count);
+			}
+
